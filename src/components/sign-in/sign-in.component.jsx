fd7846b15cd4f3f432602809ed598d3515c9f154
Koncz Gábor <gabee1987@gmail.com>
2024-01-15 11:11:24 +0100

    cleanup in sign-in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,51 +1,19 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
 import {
-  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
   signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  // This is wrong in the course ->
-  // const SignIn = () => {
-  //   useEffect(async () => {
-  //     const response = await getRedirectResult(auth);
-  //     console.log(response);
-  //   }, []);
-
-  useEffect(() => {
-    const fetchRedirectResult = async () => {
-      const response = await getRedirectResult(auth);
-      console.log(response);
-
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    };
-    fetchRedirectResult();
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
-  const logGoogleRedirectUser = async () => {
-    const { user } = await signInWithGoogleRedirect();
-    // After the redirect the following codes wont execute ->
-    console.log(user);
   };
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={logGoogleRedirectUser}>
-        Sign in with Google Redirect
-      </button>
     </div>
   );
 };
